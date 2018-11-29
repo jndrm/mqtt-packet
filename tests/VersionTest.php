b4d250c688ecb3955e930c1c2ab0c6352d5a35d7
Donald <donald@drmer.net>
2018-11-29 18:03:28 +0800

    add Version and Version4 test

---
+++ tests/VersionTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drmer\Tests\Mqtt\Packet;
+
+use Drmer\Mqtt\Packet\Protocol\Version;
+
+/**
+ *
+ * @covers \Drmer\Mqtt\Packet\Protocol\Version
+ *
+ */
+class VersionTest extends TestCase {
+    public function testExistance()
+    {
+        $this->assertTrue(interface_exists('Drmer\Mqtt\Packet\Protocol\Version'));
+    }
+}
